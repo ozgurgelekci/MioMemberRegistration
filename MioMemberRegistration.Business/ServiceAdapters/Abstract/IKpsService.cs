8149c191d111ccd284df62297041667141073311
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-16 15:58:09 +0300

    kps service implementation

---
+++ MioMemberRegistration.Business/ServiceAdapters/Abstract/IKpsService.cs
@@ -0,0 +1,9 @@
+﻿using MioMemberRegistration.Entities.Concrete;
+
+namespace MioMemberRegistration.Business.ServiceAdapters.Abstract
+{
+    public interface IKpsService
+    {
+        bool ValidateUser(Member member);
+    }
+}
---
+++ MioMemberRegistration.Business/ServiceAdapters/Concrete/KpsServiceAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MioMemberRegistration.Business.KpsServiceReference;
+using MioMemberRegistration.Business.ServiceAdapters.Abstract;
+using MioMemberRegistration.Entities.Concrete;
+
+namespace MioMemberRegistration.Business.ServiceAdapters.Concrete
+{
+    public class KpsServiceAdapter : IKpsService
+    {
+        public bool ValidateUser(Member member)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(Convert.ToInt64(member.TcNo),
+                member.FirstName.ToUpper(),
+                member.LastName.ToUpper(),
+                 member.DateOfBirth.Year);
+        }
+    }
+}
