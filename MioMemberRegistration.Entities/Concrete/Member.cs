0d583370484a932a5b2ad59db7e964500eb4a0c3
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-16 14:27:02 +0300

    added entities layer and member entity

---
+++ MioMemberRegistration.Entities/Concrete/Member.cs
@@ -0,0 +1,15 @@
+﻿using MioBlog.Core.Entities;
+using System;
+
+namespace MioMemberRegistration.Entities.Concrete
+{
+    public class Member : IEntity
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string TcNo { get; set; }
+    }
+}
