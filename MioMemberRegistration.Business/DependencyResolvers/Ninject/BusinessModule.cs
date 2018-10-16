e2589378c1841c3733da0de693554345f5a384ca
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-16 15:59:07 +0300

    dependency injection and instance factory operations

---
+++ MioMemberRegistration.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,21 @@
+﻿using MioMemberRegistration.Business.Abstract;
+using MioMemberRegistration.Business.Concrete;
+using MioMemberRegistration.Business.ServiceAdapters.Abstract;
+using MioMemberRegistration.Business.ServiceAdapters.Concrete;
+using MioMemberRegistration.DataAccess.Abstract;
+using MioMemberRegistration.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+
+namespace MioMemberRegistration.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMemberService>().To<MemberManager>().InSingletonScope();
+            Bind<IMemberDal>().To<EfMemberDal>().InSingletonScope();
+
+            Bind<IKpsService>().To<KpsServiceAdapter>().InSingletonScope();
+        }
+    }
+}
---
+++ MioMemberRegistration.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MioMemberRegistration.Business.ValidationRules.FluentValidation;
+using MioMemberRegistration.Entities.Concrete;
+using Ninject.Modules;
+
+namespace MioMemberRegistration.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Member>>().To<MemberValidator>().InSingletonScope();
+        }
+    }
+}
