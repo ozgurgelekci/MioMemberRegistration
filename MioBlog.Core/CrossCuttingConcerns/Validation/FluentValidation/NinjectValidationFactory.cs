b522ba45da8aec942350f58946b4188100e424f4
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-17 17:17:27 +0300

    validation factory for fluent validation

---
+++ MioBlog.Core/CrossCuttingConcerns/Validation/FluentValidation/NinjectValidationFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+using Ninject;
+using Ninject.Modules;
+
+namespace MioBlog.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+   public class NinjectValidationFactory:ValidatorFactoryBase
+   {
+       private IKernel _kernel;
+
+       public NinjectValidationFactory(INinjectModule module)
+       {
+           _kernel = new StandardKernel(module);
+       }
+
+       public override IValidator CreateInstance(Type validatorType)
+       {
+           return (IValidator)_kernel.TryGet(validatorType);
+       }
+    }
+}
