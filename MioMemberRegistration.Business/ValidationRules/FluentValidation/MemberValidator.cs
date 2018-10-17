425ba9005fbd32d0c964a99f32bfff9ccc162c50
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-17 19:04:06 +0300

    bug fixed

--- MioMemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
+++ MioMemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using FluentValidation;
 using MioMemberRegistration.Entities.Concrete;
 
@@ -8,6 +9,8 @@ namespace MioMemberRegistration.Business.ValidationRules.FluentValidation
     {
         public MemberValidator()
         {
+           ValidatorOptions.LanguageManager.Culture = new CultureInfo("tr");
+
             RuleFor(m => m.FirstName).NotEmpty().WithMessage("not be empty");
             RuleFor(m => m.LastName).NotEmpty().WithMessage("not be empty");
             RuleFor(m => m.DateOfBirth).NotEmpty().WithMessage("not be empty");
@@ -17,13 +20,12 @@ namespace MioMemberRegistration.Business.ValidationRules.FluentValidation
             RuleFor(m => m.Email).EmailAddress().WithMessage("must be email");
             RuleFor(m => m.DateOfBirth).LessThan(DateTime.Now).WithMessage("must be an old date");
             RuleFor(m => m.TcNo).Length(11).WithMessage("must be 11 length");
-            //RuleFor(m => m.DateOfBirth).Must(BeAValidDate).WithMessage("must be date");
+            RuleFor(m => m.DateOfBirth).Must(BeAValidDate).WithMessage("Start date is required");
         }
 
-        //private bool BeAValidDate(DateTime value)
-        //{
-        //    DateTime date;
-        //    return DateTime.TryParse(value.Year.ToString() + "-" + value.Month.ToString() + "-" + value.Day.ToString(), out date);
-        //}
+        private bool BeAValidDate(DateTime date)
+        {
+            return !date.Equals(default(DateTime));
+        }
     }
 }
